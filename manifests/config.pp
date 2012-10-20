0fd7823df54df8fd70aa804818b1c59b4bb55966
lbennett <lbennett@mimecast.com>
2012-10-20 21:09:36 +0100

    fixing some variables

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,6 +2,11 @@ class lazylibrarian::config {
 
     $lazylibrarian_data = "/usr/local/lazylibrarian-data"
     $lazylibrarian_port = extlookup("lazylibrarian_port")
+    $nzbmatrix_username = extlookup("nzbmatrix_username")
+    $nzbmatrix_apikey = extlookup("nzbmatrix_apikey")
+    $sabnzbd_host = extlookup("sabnzbd_host")
+    $sabnzbd_root = extlookup("sabnzbd_root")
+    $sabnzbd_apikey = extlookup("sabnzbd_apikey")
 
     file { "/usr/local/lazylibrarian-data":
         ensure => directory,
