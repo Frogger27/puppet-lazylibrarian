2e432cdcb9466e9919488febd97c78ce2ec58382
lbennett <lbennett@mimecast.com>
2012-10-21 11:22:43 +0100

    tweaks

--- manifests/config.pp
+++ manifests/config.pp
@@ -8,18 +8,11 @@ class lazylibrarian::config {
     $sabnzbd_root = extlookup("sabnzbd_root")
     $sabnzbd_apikey = extlookup("sabnzbd_apikey")
 
-    file { "/usr/local/lazylibrarian-data":
-        ensure => directory,
-        owner => 'lazylibrarian',
-        group => 'automators',
-        recurse => 'true'
-    }
-
-    file { "/usr/local/lazylibrarian-data/config.ini":
+    file { "/usr/local/lazylibrarian/data/config.ini":
         content => template('lazylibrarian/config.ini.erb'),
         owner => 'lazylibrarian',
         group => 'automators',
         mode => '0644',
-        require => File['/usr/local/lazylibrarian-data']
+        require => File['/usr/local/lazylibrarian']
     }
 }
